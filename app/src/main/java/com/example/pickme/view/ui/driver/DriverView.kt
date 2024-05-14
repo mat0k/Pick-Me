039355b87ae9ee02afb121e81c6dc840b2f0b467
maatouk <maatoukh181@gmail.com>
2024-05-14 11:18:33 +0300

    add import

--- app/src/main/java/com/example/pickme/view/ui/driver/DriverView.kt
+++ app/src/main/java/com/example/pickme/view/ui/driver/DriverView.kt
@@ -4,6 +4,7 @@ package com.example.pickme.view.ui.driver
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
